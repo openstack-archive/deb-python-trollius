9483f6b418fe640f063d19633396c4df82833f97
schlamar <marc.schlaich@gmail.com>
2014-01-08 11:56:30 +0100

    Prevent segfaults by raising an exception before calling the bad code.

--- asyncio/windows_utils.py
+++ asyncio/windows_utils.py
@@ -98,6 +98,7 @@ def pipe(duplex=False, overlapped=(True, True), bufsize=BUFSIZE):
             address, access, 0, _winapi.NULL, _winapi.OPEN_EXISTING,
             flags_and_attribs, _winapi.NULL)
 
+        raise RuntimeError('This triggers a segfault.')
         ov = _winapi.ConnectNamedPipe(h1, True)
         ov.GetOverlappedResult(True)
         return h1, h2
