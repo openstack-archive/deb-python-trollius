44b99d2e7b5b2a590ba96fe8c2da8ffa6597f282
Victor Stinner <victor.stinner@gmail.com>
2014-01-10 17:36:16 +0100

    improve update_tulip.sh

--- update_tulip.sh
+++ update_tulip.sh
@@ -2,3 +2,9 @@ set -e -x
 hg update default
 hg pull https://code.google.com/p/tulip/
 hg update
+hg update trollius
+set +x
+echo "Now type:"
+echo "hg merge default"
+echo "<fix conflicts>"
+echo "hg ci -m 'Merge Tulip into Trollius'"
