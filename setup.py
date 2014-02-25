836cce350eb6f8812fbfe1eecd8c7089dcabbe94
Victor Stinner <victor.stinner@gmail.com>
2014-02-25 13:02:52 +0100

    set version to 0.1.7

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ if sys.version_info < (3,):
 
 install_options = {
     "name": "trollius",
-    "version": "0.1.6",
+    "version": "0.1.7",
     "license": "Apache License 2.0",
     "author": 'Victor Stinner',
     "author_email": 'victor.stinner@gmail.com',
