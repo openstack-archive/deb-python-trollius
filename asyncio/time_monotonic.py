fa933920548d2423cd818fb88ac6457bb067a8d3
Victor Stinner <victor.stinner@gmail.com>
2014-02-26 01:27:56 +0100

    time_monotonic: emit a log on errors
    * On Windows, fix code handling ImportError
    * On UNIX, fix error handling

--- asyncio/time_monotonic.py
+++ asyncio/time_monotonic.py
@@ -10,11 +10,15 @@ the ctypes module.
 """
 import os
 import sys
+from .log import logger
+from .py33_exceptions import get_error_class
 
 __all__ = ('time_monotonic',)
 
 # default implementation: system clock (non monotonic!)
 from time import time as time_monotonic
+# the worst resolution is 15.6 ms on Windows
+time_monotonic_resolution = 0.050
 
 if os.name == "nt":
     # Windows: use GetTickCount64() or GetTickCount()
@@ -23,39 +27,39 @@ if os.name == "nt":
         from ctypes import windll
         from ctypes.wintypes import DWORD
     except ImportError:
-        pass
-
-    # GetTickCount64() requires Windows Vista, Server 2008 or later
-    if hasattr(windll.kernel32, 'GetTickCount64'):
-        ULONGLONG = ctypes.c_uint64
-
-        GetTickCount64 = windll.kernel32.GetTickCount64
-        GetTickCount64.restype = ULONGLONG
-        GetTickCount64.argtypes = ()
-
-        def time_monotonic():
-            return GetTickCount64() * 1e-3
-        time_monotonic_resolution = 1e-3
+        logger.error("time_monotonic import error", exc_info=True)
     else:
-        GetTickCount = windll.kernel32.GetTickCount
-        GetTickCount.restype = DWORD
-        GetTickCount.argtypes = ()
+        # GetTickCount64() requires Windows Vista, Server 2008 or later
+        if hasattr(windll.kernel32, 'GetTickCount64'):
+            ULONGLONG = ctypes.c_uint64
 
-        # Detect GetTickCount() integer overflow (32 bits, roll-over after 49.7
-        # days). It increases an internal epoch (reference time) by 2^32 each
-        # time that an overflow is detected. The epoch is stored in the
-        # process-local state and so the value of time_monotonic() may be
-        # different in two Python processes running for more than 49 days.
-        def time_monotonic(use_info):
-            ticks = GetTickCount()
-            if ticks < time_monotonic.last:
-                # Integer overflow detected
-                time_monotonic.delta += 2**32
-            time_monotonic.last = ticks
-            return (ticks + time_monotonic.delta) * 1e-3
-        time_monotonic.last = 0
-        time_monotonic.delta = 0
-        time_monotonic_resolution = 1e-3
+            GetTickCount64 = windll.kernel32.GetTickCount64
+            GetTickCount64.restype = ULONGLONG
+            GetTickCount64.argtypes = ()
+
+            def time_monotonic():
+                return GetTickCount64() * 1e-3
+            time_monotonic_resolution = 1e-3
+        else:
+            GetTickCount = windll.kernel32.GetTickCount
+            GetTickCount.restype = DWORD
+            GetTickCount.argtypes = ()
+
+            # Detect GetTickCount() integer overflow (32 bits, roll-over after 49.7
+            # days). It increases an internal epoch (reference time) by 2^32 each
+            # time that an overflow is detected. The epoch is stored in the
+            # process-local state and so the value of time_monotonic() may be
+            # different in two Python processes running for more than 49 days.
+            def time_monotonic(use_info):
+                ticks = GetTickCount()
+                if ticks < time_monotonic.last:
+                    # Integer overflow detected
+                    time_monotonic.delta += 2**32
+                time_monotonic.last = ticks
+                return (ticks + time_monotonic.delta) * 1e-3
+            time_monotonic.last = 0
+            time_monotonic.delta = 0
+            time_monotonic_resolution = 1e-3
 
 elif sys.platform == 'darwin':
     # Mac OS X: use mach_absolute_time() and mach_timebase_info()
@@ -64,6 +68,7 @@ elif sys.platform == 'darwin':
         import ctypes.util
         libc_name = ctypes.util.find_library('c')
     except ImportError:
+        logger.error("time_monotonic import error", exc_info=True)
         libc_name = None
     if libc_name:
         libc = ctypes.CDLL(libc_name, use_errno=True)
@@ -101,6 +106,7 @@ elif sys.platform.startswith(("linux", "freebsd", "openbsd", "sunos")):
         import ctypes
         import ctypes.util
     except ImportError:
+        logger.error("time_monotonic import error", exc_info=True)
         libraries = ()
     else:
         if sys.platform.startswith(("freebsd", "openbsd")):
@@ -136,13 +142,17 @@ elif sys.platform.startswith(("linux", "freebsd", "openbsd", "sunos")):
         clock_gettime.argtypes = (clockid_t, timespec_p)
         clock_gettime.restype = ctypes.c_int
 
+        def ctypes_oserror():
+            errno = ctypes.get_errno()
+            message = os.strerror(errno)
+            error_class = get_error_class(errno, OSError)
+            return error_class(errno, message)
+
         def time_monotonic():
             ts = timespec()
             err = clock_gettime(time_monotonic.clk_id, ctypes.byref(ts))
             if err:
-                errno = ctypes.get_errno()
-                message = os.strerror(errno)
-                return OSError(errno, message)
+                raise ctypes_oserror()
             return ts.tv_sec + ts.tv_nsec * 1e-9
 
         if sys.platform.startswith("linux"):
@@ -167,4 +177,7 @@ elif sys.platform.startswith(("linux", "freebsd", "openbsd", "sunos")):
             return ts.tv_sec + ts.tv_nsec * 1e-9
         time_monotonic_resolution = get_resolution()
         del get_resolution
+
+else:
+    logger.error("time_monotonic: unspported platform %r", sys.platform)
 
