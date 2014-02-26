38d8a0aeb75138d5799535cbb17459f2842bef87
Victor Stinner <victor.stinner@gmail.com>
2014-02-26 12:33:08 +0100

    Add docstring to py3_ssl.py

--- asyncio/py3_ssl.py
+++ asyncio/py3_ssl.py
@@ -1,3 +1,12 @@
+"""
+Backport SSL functions and exceptions:
+- BACKPORT_SSL_ERRORS (bool)
+- SSLWantReadError, SSLWantWriteError, SSLEOFError
+- BACKPORT_SSL_CONTEXT (bool)
+- SSLContext
+- wrap_socket()
+- wrap_ssl_error()
+"""
 import errno
 import ssl
 import sys
