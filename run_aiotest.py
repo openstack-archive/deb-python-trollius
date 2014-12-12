9ebebb68b3bf5707d652248a7e974c4600751263
Victor Stinner <victor.stinner@gmail.com>
2014-12-12 16:31:29 +0100

    Add run_aiotest.py

---
+++ run_aiotest.py
@@ -0,0 +1,9 @@
+import aiotest.run
+import trollius
+
+config = aiotest.TestConfig()
+config.asyncio = trollius
+config.new_event_pool_policy = trollius.DefaultEventLoopPolicy
+config.call_soon_check_closed = True
+#aiotest.run.main(config)
+aiotest.run.runtests(config)
